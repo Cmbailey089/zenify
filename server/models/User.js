bf08d429f950683cbe2d3255fd742deca863c8d6
CorbinSpence <corbinspence16@gmail.com>
2023-06-29 19:54:00 -0400

    fixed typeDefs and added functions to User

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   username: {
@@ -16,7 +17,28 @@ const userSchema = new Schema({
     unique: true,
     match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
   },
+},
+ // set this to use virtual below
+ {
+  toJSON: {
+    virtuals: true,
+  },
+}
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
 });
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
