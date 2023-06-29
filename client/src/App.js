6c67e563a29489f2baa5f4fc14f715ecb6a4cd91
Jacob Sniff <jacobsniff18@gmail.com>
2023-06-29 19:55:04 -0400

    Navbar has been made with some minor styling and routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path='/' exact component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/sign-up' component={SignUp} />
+    </Router>
   );
 }
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const SignUp = () => {
 return (
 	<div
 	style={{
@@ -10,9 +10,9 @@ return (
 		height: '100vh'
 	}}
 	>
-	<h1>Welcome to Zenify</h1>
+	<h1>Sign Up</h1>
 	</div>
 );
 };
 
-export default Home;
+export default SignUp;
