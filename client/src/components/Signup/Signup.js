6026f249298e06e91e61c4ee28578b26666ad6f3
Gabriela <gabriela.reyna89@icloud.com>
2023-07-10 12:51:05 -0400

    adding styling and logic to navbar

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,40 +1,44 @@
 import React, { useState } from 'react';
-import './Signupstyles.css'
+import './Signupstyles.css';
 
 const SignUp = () => {
-	const [username, setUsername] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-  
-	// ...form submission and validation logic can be added here
-  
-	return (
-	  <div>
-		<form>
-		  <h1>Sign Up</h1>
-		  <input
-			type="text"
-			placeholder="Username"
-			value={username}
-			onChange={(e) => setUsername(e.target.value)}
-		  />
-		  <input
-			type="email"
-			placeholder="Email"
-			value={email}
-			onChange={(e) => setEmail(e.target.value)}
-		  />
-		  <input
-			type="password"
-			placeholder="Password"
-			value={password}
-			onChange={(e) => setPassword(e.target.value)}
-		  />
-		  <button type="submit">Sign Up</button>
-		</form>
-	  </div>
-	);
-  };
-  
-  export default SignUp;
-  
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // ...form submission and validation logic can be added here
+
+  return (
+    <div className="signup-container">
+      <form className="signup-form">
+        <h1 className="signup-title">Sign Up</h1>
+        <input
+          className="signup-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="signup-submit-btn" type="submit">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
