fb3504207bb68cbc03d1d76407ddfa235d3a68dc
Gabriela <gabriela.reyna89@icloud.com>
2023-07-10 12:27:45 -0400

    adding branch

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Home = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Welcome to Zenify</h1>
-	</div>
-);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#f2f2f2',
+      }}
+    >
+      <h1>Welcome to Zenify</h1>
+    </div>
+  );
 };
 
 export default Home;
