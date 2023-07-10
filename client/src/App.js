6af392cbe7e88023919ff91feb80ff1158f4da8b
Gabriela <gabriela.reyna89@icloud.com>
2023-07-10 14:44:11 -0400

    importing transition/switch adding it ti content to router, route

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,37 @@ import About from "./pages/About";
 import SignUp from "./components/Signup/Signup";
 import Videos from "./components/Videos/Videos";
 import Footer from "./components/Footer/Footer";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    <Router>
-      <Navbar />
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/videos" component={Videos} />
-      <Route path="/sign-up" component={SignUp} />
-    </Router>
-    <Footer />
+      <Router>
+        <Navbar />
+        <Route
+          render={({ location }) => {
+            const locationKey = location.pathname; // Use the pathname as the key
+            return (
+              <TransitionGroup>
+                <CSSTransition
+                  key={locationKey}
+                  timeout={500}
+                  classNames="page-transition"
+                >
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/videos" component={Videos} />
+                    <Route path="/sign-up" component={SignUp} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            );
+          }}
+        />
+      </Router>
+      <Footer />
     </div>
   );
 }
