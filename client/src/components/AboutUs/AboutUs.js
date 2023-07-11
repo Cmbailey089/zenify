5a9a73c611aae3fcb152f87144aedcc980d8b6ff
Gabriela <gabriela.reyna89@icloud.com>
2023-07-11 19:59:43 -0400

    added a new pckg, icon and social media icons

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import backgroundImg from '../AboutUs/codimg.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './AboutUsStyles.css';
+import gabrielaImg from './Icons/Gabriela.png';
 
 const AboutUs = () => {
   return (
@@ -43,13 +46,22 @@ const AboutUs = () => {
         </div>
       </div>
       <div className="Member">
-        <div className="card">
-          <h3 className="Gabriela">Gabriela</h3>
-          <p className="answer">
-            {/* Gabriela info would go here */}
-          </p>
-        </div>
-      </div>
+      <div className="card">
+  <h3 className="Gabriela">Gabriela</h3>
+  <img src={gabrielaImg} alt="Gabriela" className="profile-img" />
+  <div className="social-media-icons">
+    <a href="https://twitter.com/gabriela" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faTwitter} className="social-icon" /> Twitter
+    </a>
+    <a href="https://instagram.com/gabriela" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faInstagram} className="social-icon" /> Instagram
+    </a>
+    <a href="https://linkedin.com/in/gabriela" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faLinkedin} className="social-icon" /> Linkedin
+    </a>
+  </div>
+</div>
+</div>
       {/* Add more Members here */}
     </div>
   );
