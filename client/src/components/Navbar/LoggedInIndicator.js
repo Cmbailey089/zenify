421df82597f945c6c9733ac032d15590f9fc43af
Gabriela <gabriela.reyna89@icloud.com>
2023-07-11 16:22:55 -0400

    added simple design for the logged in indicator

--- client/src/components/Navbar/LoggedInIndicator.js
+++ client/src/components/Navbar/LoggedInIndicator.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './LoggedInIndicator.css';
 
 const LoggedInIndicator = ({ username }) => {
   console.log('Username:', username);
 
   return (
     <div className="loggedin-indicator">
-      <p>Logged in as {username}</p>
+      <p>Logged in as: {username}</p>
     </div>
   );
 };
