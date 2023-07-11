b9748ccc10eba7b2900057e32e78c4769f872b87
John Metzger <jsmetzger9@gmail.com>
2023-07-10 20:38:36 -0400

    updated animated logo with transparent gif

--- client/src/components/Navbar/VideoLogo.js
+++ client/src/components/Navbar/VideoLogo.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const VideoLogo = () => {
   return (
-    <video autoPlay loop muted>
-      <source src="/media/VideoLogo.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <img src="/media/VideoLogo.gif" alt="Video Logo" />
   );
 }
 
