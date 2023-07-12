c0fb1e0a78780fd0cc9136490763de3d93d5b5d9
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-12 17:11:01 -0400

    added link to cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,18 +19,18 @@ const Home = ({ loggedIn }) => {
         )}
         {loggedIn && (
           <div className="features">
-            <div className="feature-item">
+            <Link to='/tips' className="feature-item">
               <h3>Stress Management Tips</h3>
               <p>Explore helpful tips and strategies to effectively manage stress in your life.</p>
-            </div>
-            <div className="feature-item">
+            </Link>
+            <Link to='meditation' className="feature-item">
               <h3>Guided Meditation</h3>
               <p>Experience guided meditation sessions to relax your mind and reduce stress levels.</p>
-            </div>
-            <div className="feature-item">
+            </Link>
+            <Link to='/breathing' className="feature-item">
               <h3>Breathing Exercises</h3>
               <p>Learn different breathing techniques that can help calm your mind and relieve stress.</p>
-            </div>
+            </Link>
           </div>
         )}
       </div>
