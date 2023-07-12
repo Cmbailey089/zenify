9db83073865eae459cf3fc49bb4913cfce753f67
Gabriela <gabriela.reyna89@icloud.com>
2023-07-12 16:44:04 -0400

    changin fonts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,18 +19,18 @@ const Home = ({ loggedIn }) => {
         )}
         {loggedIn && (
           <div className="features">
-            <Link to="/tips" className="feature-item">
+            <div className="feature-item">
               <h3>Stress Management Tips</h3>
               <p>Explore helpful tips and strategies to effectively manage stress in your life.</p>
-            </Link>
-            <Link to="/meditation" className="feature-item">
+            </div>
+            <div className="feature-item">
               <h3>Guided Meditation</h3>
               <p>Experience guided meditation sessions to relax your mind and reduce stress levels.</p>
-            </Link>
-            <Link to="breathing" className="feature-item">
+            </div>
+            <div className="feature-item">
               <h3>Breathing Exercises</h3>
               <p>Learn different breathing techniques that can help calm your mind and relieve stress.</p>
-            </Link>
+            </div>
           </div>
         )}
       </div>
