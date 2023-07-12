9d6d554d3577b604d2a50708624fed8490258b4b
Gabriela <gabriela.reyna89@icloud.com>
2023-07-11 22:24:12 -0400

    adding Chris icon to team page

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './AboutUsStyles.css';
 import gabrielaImg from './Icons/Gabriela.png';
+import chrisImg from './Icons/Chris.png';
 
 const AboutUs = () => {
   return (
@@ -14,10 +15,19 @@ const AboutUs = () => {
       <h2>Team</h2>
       <div className="Member">
         <div className="card">
-          <h3 className="Christopher">Christoper</h3>
-          <p className="answer">
-            {/* Christoper info would go here */}
-          </p>
+          <h3 className="Christopher">Christopher</h3>
+          <img src={chrisImg} alt="Christopher" className="profile-img" />
+          <div className="social-media-icons">
+            <a href="https://twitter.com/Christopher" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} className="social-icon" /> Twitter
+            </a>
+            <a href="https://instagram.com/Christopher" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="social-icon" /> Instagram
+            </a>
+            <a href="https://linkedin.com/in/Christopher" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="social-icon" /> Linkedin
+            </a>
+          </div>
         </div>
       </div>
       <div className="Member">
@@ -46,22 +56,22 @@ const AboutUs = () => {
         </div>
       </div>
       <div className="Member">
-      <div className="card">
-  <h3 className="Gabriela">Gabriela</h3>
-  <img src={gabrielaImg} alt="Gabriela" className="profile-img" />
-  <div className="social-media-icons">
-    <a href="https://twitter.com/gabriela" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={faTwitter} className="social-icon" /> Twitter
-    </a>
-    <a href="https://instagram.com/gabriela" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={faInstagram} className="social-icon" /> Instagram
-    </a>
-    <a href="https://linkedin.com/in/gabriela" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={faLinkedin} className="social-icon" /> Linkedin
-    </a>
-  </div>
-</div>
-</div>
+        <div className="card">
+          <h3 className="Gabriela">Gabriela</h3>
+          <img src={gabrielaImg} alt="Gabriela" className="profile-img" />
+          <div className="social-media-icons">
+            <a href="https://twitter.com/gabriela" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} className="social-icon" /> Twitter
+            </a>
+            <a href="https://instagram.com/gabriela" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="social-icon" /> Instagram
+            </a>
+            <a href="https://linkedin.com/in/gabriela" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="social-icon" /> Linkedin
+            </a>
+          </div>
+        </div>
+      </div>
       {/* Add more Members here */}
     </div>
   );
