83d99bb49d4dfccb82eb0c6e80691e08376268aa
John Metzger <jsmetzger9@gmail.com>
2023-07-13 17:35:36 -0400

    Update and delete user functionality

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const {
   createUser,
   getSingleUser,
-  /* saveBook,
-  deleteBook, */
+  updateUser,
+  deleteUser,
   login,
 } = require('../../controllers/user-controller');
 
@@ -13,6 +13,8 @@ const { authMiddleware } = require('../../utils/auth');
 router.route('/').post(createUser);
 router.route('/login').post(login);
 router.route('/user').get(authMiddleware, getSingleUser);
-// router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+router.route('/user/:id').put(authMiddleware, updateUser);
+router.route('/user/:id').delete(authMiddleware, deleteUser);
 
 module.exports = router;
+
